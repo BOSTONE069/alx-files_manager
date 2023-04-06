fb8d142f2269999d01d9785040e062c50cf3d6c9
BOSTONE069 <bostoneochieng69@gmail.com>
2023-04-06 09:49:43 -0400

    This is the express js server controllers connections through

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
-import UsersController from '../controllers/UsersController';
-import AuthController from '../controllers/AuthController';
-import FilesController from '../controllers/FilesController';
+
 
 function controllerRouting(app) {
   const router = express.Router();
