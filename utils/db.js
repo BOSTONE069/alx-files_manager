2a17f8e8af01970bfd0975bbbcbcfbfd4a9a9302
BOSTONE069 <bostoneochieng69@gmail.com>
2023-04-06 09:08:40 -0400

    Mongo DB connection code

---
+++ utils/db.js
@@ -0,0 +1,57 @@
+import { MongoClient } from 'mongodb';
+
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${DB_HOST}:${DB_PORT}`;
+
+/**
+ * Class for performing operations with Mongo service
+ */
+class DBClient {
+  constructor() {
+    this.connect();
+  }
+
+  async connect() {
+    try {
+      const client = await MongoClient.connect(url, { useUnifiedTopology: true });
+      this.db = client.db(DB_DATABASE);
+      this.usersCollection = this.db.collection('users');
+      this.filesCollection = this.db.collection('files');
+    } catch (err) {
+      console.log(err.message);
+      this.db = false;
+    }
+  }
+
+  /**
+   * Checks if connection to MongoDB is alive
+   * @return {boolean} true if connection alive or false if not
+   */
+  isAlive() {
+    return Boolean(this.db);
+  }
+
+  /**
+   * Returns the number of documents in the collection users
+   * @return {number} amount of users
+   */
+  async nbUsers() {
+    const numberOfUsers = await this.usersCollection.countDocuments();
+    return numberOfUsers;
+  }
+
+  /**
+   * Returns the number of documents in the collection files
+   * @return {number} amount of files
+   */
+  async nbFiles() {
+    const numberOfFiles = await this.filesCollection.countDocuments();
+    return numberOfFiles;
+  }
+}
+
+const dbClient = new DBClient();
+
+export default dbClient;
