57b1776c8df1a7d5f020ad8d393d8d098047c224
BOSTONE069 <bostoneochieng69@gmail.com>
2023-04-06 12:01:33 -0400

    Added the user controllers and the users end points

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
-
+import UsersController from '../controllers/UsersController';
 
 function controllerRouting(app) {
   const router = express.Router();
